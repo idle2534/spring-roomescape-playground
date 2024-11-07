368622c24734ffa6dc770d33d55bc45164b6dca9
IDLE <83659656+idle2534@users.noreply.github.com>
2024-11-07 18:05:36 +0900

    Feat: Time 홈페이지 thymleaf 코드 추가

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,11 @@ import roomescape.service.TimeService;
 public class TimeController {
 
   private final TimeService timeService;
+
+  @GetMapping("/time")
+  public String time() {
+    return "time";
+  }
 
   @GetMapping("/times")
   public ResponseEntity<List<Time>> getTimes() {
